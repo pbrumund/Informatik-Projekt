a5ab68c94d7791f18c9197f0ff3eef199d58539c
Patrick Brumund <patrickbrumund@gmail.com>
2018-05-16 16:17:02 +0200

    Kommentare

--- Button_command_test.py
+++ Button_command_test.py
@@ -4,44 +4,44 @@ from tkinter import ttk
 
 Buttons= []
 cur_button= 0
-class Keypad(object):
+class Keypad(object):   #Erzeugt die Buttons des Keypads
      def __init__(self, Window):
-        self.keys=[
+        self.keys=[     #Beschriftungen der Buttons
             ['1','2','3','A'],
             ['4','5','6','B'],
             ['7','8','9','C'],
             ['*','0','#','D'] 
         ]
-        for y in range(4):
+        for y in range(4):      #Erzeugt die Buttons
             for x in range(4):
-                Buttons.append(Button(name= self.keys[y][x], x=x, y=y, index=x+(4*y)))
+                Buttons.append(Button(name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
         
 class Button (object):
     def __init__(self, name, x, y, index, button_text= ''):
-        self.button=tk.ttk.Button(Window, command= self.set_cur_button, text= name)
-        self.button.grid(row= y, column= x)
-        self.button_text= button_text
-        self.index= index
-    def change_text(self, new_text):
+        self.button=tk.ttk.Button(Window, command= self.set_cur_button, text= name) #Erzeugt Button über tkinter
+        self.button.grid(row= y, column= x)     
+        self.button_text= button_text   #geespeicherter Text, der geändert werden kann
+        self.index= index   #Position in der Liste
+    def change_text(self, new_text):    #ändert den Text
         self.button_text= new_text
     #def print_text(self):
      #   print (self.button_text)
     def exec_command(self):
         pass    
-    def get_text(self):
+    def get_text(self):     #Gibt den gespeicherten Text aus
         return self.button_text
-    def set_cur_button(self):
+    def set_cur_button(self):      #Setzt den angeklickten Button als aktuell ausgewählten Button, der bearbeitet werden soll
         global cur_button
         cur_button=self.index
-        Text_field.update_text(self.button_text)
-        Text_field.text_field.icursor(0)
+        Text_field.update_text(self.button_text)      #Zeigt den Text des ausgewählten Buttons im Textfeld an
+        #Text_field.text_field.icursor(0)
         
 
-class Save_button (object):
-    def __init__(self, Window):
+class Save_button (object):     #Button zum speichern
+    def __init__(self, Window):     #Erzeut Button
         save_button= tk.ttk.Button(Window, command= Save_button.save_text, text= 'Save')
         save_button.grid(row=5, column=2)
-    def save_text():
+    def save_text():       #Speichert den eingegebenen Text im Button
         global cur_button
         text= Text_field.text_field.get()
         Buttons[cur_button].change_text(text)
@@ -51,8 +51,8 @@ class Text_field (object):
         self.text_field= tk.Entry(Window) #Entry ist im Gegensatz zu Text von Anfang an sichtbar.
         self.text_field.grid(row= 5, column= 0, columnspan= 2)
     def update_text(self, text):# Ändert den gegebenen Text
-        self.text_field.delete(0,'end')
-        self.text_field.insert(0,text)
+        self.text_field.delete(0,'end') #Leert das Feld
+        self.text_field.insert(0,text)  #Neuer Text
 
 class Text_field2 (object):
     def __init__(self, Window):
@@ -68,10 +68,11 @@ class Submit_button (object):
         pass #noch keine Funktion
     
     
-Window= tk.Tk()
+Window= tk.Tk()     #Erzeut Fenster
 Window.title('Keypad')
-Keypad= Keypad(Window)
-Save_button= Save_button(Window)
+
+Keypad= Keypad(Window)  #Erzeugt Keypad
+Save_button= Save_button(Window)  
 #Submit_button=Submit_button(Window)
 label1 = tk.Label(Window, text="Neue Belegung für Taste:")
 label1.grid(row=4,column=0, columnspan= 2)
