7d4f18144d2e539973bc69827de8330f3e19dbb7
Patrick Brumund <patrickbrumund@gmail.com>
2018-05-23 18:30:30 +0200

    Window und Arduino sind jetzt Objekte

--- Button_command_test.py
+++ Button_command_test.py
@@ -2,27 +2,51 @@ import tkinter as tk
 import pyautogui as key
 import serial
 from tkinter import ttk
+import socket as so
 
 Buttons= []
 cur_button= 0
-def call_function(event):
-    pass
+
+not_working_keys= ['ü','ö','ä','Ü','Ö','Ä','^','ß','´','°','§','²','³','{','}','+','~','#','@','€','<','|']
+windows_commands= ['ü','ö','ä','Ü','Ö','','','','','^','','ctrl alt 2','ctrl alt 3','ctrl alt 7','ctrl alt 0','','ctrl alt +',
+'','ctrl alt q','ctrl alt e','','ctrl alt <']
+
+key._pyautogui_win.keyboardMapping.update({'+': 0x6b})
+
+#arduino = serial.Serial('COM7',9600,timeout=0.1)
+
+class Arduino(object):
+    def __init__(self, window):
+        self.arduino = serial.Serial('COM7',9600,timeout=0.1)
+        self.window= window
 
 
-arduino = serial.Serial('COM3',9600,timeout=0.1)
-
-def listen():
-    read = arduino.readline()
-    if (read):
-        print("Aktion registriert")
-        print(read)
-    Window.after(10,listen)
+    def listen(self):
+        read = self.arduino.readline()
+        if (read):
+            i= int(read)-1
+            Buttons[i].exec_command()
+        self.window.after(10,self.listen)
         
 
+class Window(object):
+    def __init__(self):
+        self.window= tk.Tk()
+                
+        self.label1 = tk.Label(self.window, text="Neue Belegung für Taste:")
+        self.label1.grid(row=4,column=0, columnspan= 2)
+        self.text_field= Text_field(self.window)
+        self.save_button= Save_button(self.window, self.text_field)  
+        self.keypad= Keypad(self.window, self.text_field)  #Erzeugt Keypad
 
+        self.arduino= Arduino(self.window)
+        self.arduino.listen()
+        #listen(self.window)
+        self.window.mainloop()
+        
 
 class Keypad(object):   #Erzeugt die Buttons des Keypads
-     def __init__(self, Window):
+     def __init__(self, window, text_field):
         self.keys=[     #Beschriftungen der Buttons
             ['1','2','3','A'],
             ['4','5','6','B'],
@@ -31,39 +55,54 @@ class Keypad(object):   #Erzeugt die Buttons des Keypads
         ]
         for y in range(4):      #Erzeugt die Buttons
             for x in range(4):
-                Buttons.append(Button(name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
+                Buttons.append(Button(window, text_field, name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
         
 class Button (object):
-    def __init__(self, name, x, y, index, button_text= ''):
+    def __init__(self, window, text_field, name, x, y, index, button_text= ''):
         style= tk.ttk.Style()
         style.configure("Active_Button.TButton", foreground="black", background="blue")
-        self.button=tk.ttk.Button(Window, command= self.set_cur_button, text= name) #Erzeugt Button über tkinter
+        self.button=tk.ttk.Button(window, command= self.set_cur_button, text= name) #Erzeugt Button über tkinter
         self.button.grid(row= y, column= x)     
         self.button_text= button_text   #geespeicherter Text, der geändert werden kann
         self.index= index   #Position in der Liste
+        self.window= window
+        self.text_field= text_field
+
     def change_text(self, new_text):    #ändert den Text
         self.button_text= new_text
-    #def print_text(self):
-     #   print (self.button_text)
+        
     def exec_command(self):
-        pass    
+        print(self.button_text)    
+        key.typewrite(self.button_text, interval= 0.01)
+        """for letter in self.button_text:
+            if letter=='{':
+                key.hotkey('ctrl','alt','7')
+            elif letter=='}':
+                key.hotkey('ctrl','alt','0')
+            else:
+                key.press(letter,pause=0.01)
+            #time.sleep(0.01)"""
+
     def get_text(self):     #Gibt den gespeicherten Text aus
         return self.button_text
+
     def set_cur_button(self):      #Setzt den angeklickten Button als aktuell ausgewählten Button, der bearbeitet werden soll
         global cur_button
         Buttons[cur_button].button.configure(style= "TButton")
         cur_button=self.index
         Buttons[cur_button].button.configure(style= "Active_Button.TButton")
-        Text_field.update_text(self.button_text)      #Zeigt den Text des ausgewählten Buttons im Textfeld an
+        self.text_field.update_text(self.button_text)      #Zeigt den Text des ausgewählten Buttons im Textfeld an
               
 
 class Save_button (object):     #Button zum speichern
-    def __init__(self, Window):     #Erzeut Button
-        save_button= tk.ttk.Button(Window, command= Save_button.save_text, text= 'Save')
+    def __init__(self, window, text_field):     #Erzeut Button
+        save_button= tk.ttk.Button(window, command= self.save_text, text= 'Save')
         save_button.grid(row=5, column=2)
-    def save_text():       #Speichert den eingegebenen Text im Button
+        self.text_field= text_field
+
+    def save_text(self):       #Speichert den eingegebenen Text im Button
         global cur_button
-        text= Text_field.text_field.get()
+        text= self.text_field.text_field.get()
         Buttons[cur_button].change_text(text)
         
 class Text_field (object):
@@ -74,27 +113,20 @@ class Text_field (object):
         self.text_field.delete(0,'end') #Leert das Feld
         self.text_field.insert(0,text)  #Neuer Text
 
-class Text_field2 (object):
-    def __init__(self, Window):
-        text_field2= tk.Entry(Window) 
-        text_field2.grid(row= 3, column= 4)
-
 
     
-    
-Window= tk.Tk()     #Erzeut Fenster
-Window.title('Keypad')
+window= Window()  
+#Window= tk.Tk()     #Erzeut Fenster
+#Window.title('Keypad')
 
 
 
-Keypad= Keypad(Window)  #Erzeugt Keypad
-Save_button= Save_button(Window)  
-#Submit_button=Submit_button(Window)
-label1 = tk.Label(Window, text="Neue Belegung für Taste:")
-label1.grid(row=4,column=0, columnspan= 2)
-#label2 = tk.Label(Window, text="Zu verändernde Taste:")
-#label2.grid(row=2,column=4)
-Text_field= Text_field(Window)
-#Text_field2= Text_field2(Window)
-listen()
-Window.mainloop()
+#Keypad= Keypad(Window)  #Erzeugt Keypad
+#Save_button= Save_button(Window)  
+#label1 = tk.Label(Window, text="Neue Belegung für Taste:")
+#label1.grid(row=4,column=0, columnspan= 2)
+#Text_field= Text_field(Window)
+#Buttons[1].button_text= "{}"
+
+#listen()
+#Window.mainloop()
