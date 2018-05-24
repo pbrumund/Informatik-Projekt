6902a0f2ac1bacd952e9a5abd873557ac15b63df
Patrick Brumund <patrickbrumund@gmail.com>
2018-05-24 14:03:56 +0200

    checkbox für tastenkombinationen

--- Keypad_GUI.py
+++ Keypad_GUI.py
@@ -28,8 +28,9 @@ class Window(object):
         self.label1 = tk.Label(self.window, text="Neue Belegung für Taste:")
         self.label1.grid(row=4,column=0, columnspan= 2)
         self.text_field= Text_field(self.window)
-        self.save_button= Save_button(self.window, self.text_field)  
-        self.keypad= Keypad(self.window, self.text_field)  #Erzeugt Keypad
+        self.command_checkbutton= Command_checkbutton(self.window)
+        self.save_button= Save_button(self.window, self.text_field, self.command_checkbutton)
+        self.keypad= Keypad(self.window, self.text_field, self.command_checkbutton)  #Erzeugt Keypad
 
         self.arduino= Arduino(self.window)
         self.arduino.listen()
@@ -39,7 +40,7 @@ class Window(object):
             
 
 class Keypad(object):   #Erzeugt die Buttons des Keypads
-     def __init__(self, window, text_field):
+     def __init__(self, window, text_field, command_checkbutton):
         self.keys=[     #Beschriftungen der Buttons
             ['1','2','3','A'],
             ['4','5','6','B'],
@@ -48,21 +49,24 @@ class Keypad(object):   #Erzeugt die Buttons des Keypads
         ]
         for y in range(4):      #Erzeugt die Buttons
             for x in range(4):
-                Buttons.append(Button(window, text_field, name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
+                Buttons.append(Button(window, text_field, command_checkbutton, name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
         
 class Button (object):
-    def __init__(self, window, text_field, name, x, y, index, button_text= ''):
+    def __init__(self, window, text_field, command_checkbutton, name, x, y, index):
         style= tk.ttk.Style()
         style.configure("Active_Button.TButton", foreground="black", background="blue")
         self.button=tk.ttk.Button(window, command= self.set_cur_button, text= name) #Erzeugt Button über tkinter
         self.button.grid(row= y, column= x)     
-        self.button_text= button_text   #geespeicherter Text, der geändert werden kann
+        self.button_text= ''   #geespeicherter Text, der geändert werden kann
         self.index= index   #Position in der Liste
         self.window= window
         self.text_field= text_field
+        self.command_checkbutton= command_checkbutton
+        self.is_command= 0
 
-    def change_text(self, new_text):    #ändert den Text
+    def change_text(self, new_text, is_command):    #ändert den Text
         self.button_text= new_text
+        self.is_command= is_command
         
     def exec_command(self):
         key.typewrite(self.button_text, interval= 0.00)
@@ -76,28 +80,36 @@ class Button (object):
         cur_button=self.index
         Buttons[cur_button].button.configure(style= "Active_Button.TButton")
         self.text_field.update_text(self.button_text)      #Zeigt den Text des ausgewählten Buttons im Textfeld an
-              
+        self.command_checkbutton.update_checked(self.is_command)
 
 class Save_button (object):     #Button zum speichern
-    def __init__(self, window, text_field):     #Erzeut Button
+    def __init__(self, window, text_field, command_checkbutton):     #Erzeut Button
         save_button= tk.ttk.Button(window, command= self.save_text, text= 'Save')
         save_button.grid(row=5, column=2)
         self.text_field= text_field
+        self.command_checkbutton= command_checkbutton
 
     def save_text(self):       #Speichert den eingegebenen Text im Button
         global cur_button
         text= self.text_field.text_field.get()
-        Buttons[cur_button].change_text(text)
+        is_command= self.command_checkbutton.checked.get()
+        Buttons[cur_button].change_text(text, is_command)
+
         
 class Text_field (object):
-    def __init__(self, Window):
-        self.text_field= tk.Entry(Window) #Entry ist im Gegensatz zu Text von Anfang an sichtbar.
+    def __init__(self, window):
+        self.text_field= tk.Entry(window) #Entry ist im Gegensatz zu Text von Anfang an sichtbar.
         self.text_field.grid(row= 5, column= 0, columnspan= 2)
         
     def update_text(self, text):# Ändert den gegebenen Text
         self.text_field.delete(0,'end') #Leert das Feld
         self.text_field.insert(0,text)  #Neuer Text
 
-
-    
+class Command_checkbutton(object):
+    def __init__(self, window):
+        self.checked= tk.IntVar()
+        self.checkbutton= tk.ttk.Checkbutton(window, text= 'Command', variable= self.checked)
+        self.checkbutton.grid(row=5, column= 3)
+    def update_checked(self, checked):
+        self.checked.set(checked)
 window= Window()  
