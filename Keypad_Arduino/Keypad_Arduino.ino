6bc4c41043ab90abd0078760697bda8699c24566
Patrick Brumund <patrickbrumund@gmail.com>
2018-06-04 13:58:05 +0200

    aktuellste version macht nicht wirklich was es soll

--- Keypad_Arduino/Keypad_Arduino.ino
+++ Keypad_Arduino/Keypad_Arduino.ino
@@ -7,6 +7,7 @@
 const byte ROWS = 4;
 const byte COLS = 4;
 unsigned long next_send = 0;
+bool closed= 0;
 
 uint8_t keys[ROWS][COLS] = {
   {1, 2, 3, 4},
@@ -33,7 +34,7 @@ void setup()
 
 
   /* Get and print the IP-Address the python program
-   * should connect to */
+     should connect to */
   char ip[16];
   esp_server.my_ip(ip, 16);
 
@@ -43,22 +44,47 @@ void setup()
 
 void loop()
 {
-  if(millis()>next_send){
-    uint8_t key = kpd.getKey();
-    bool curr_connected= true;//esp_server.connected();
-    if (key && curr_connected) // Check for a valid key.
-    {
-     esp_server.println(key);
-     Serial.println(key);
+  uint8_t key = kpd.getKey();
+  bool curr_connected = esp_server.connected();
+  //Serial.println(curr_connected);
+  //Serial.println(next_send);
+  while(esp_server.available()){
+    String msg = esp_server.readStringUntil('\n');
+    Serial.println('Read message');
+    if(msg=="off"){
+      closed = 1;
+      next_send = millis() + 200;
+      Serial.println("Tried to close the connection");
     }
-    else if (curr_connected)
-    {
-     esp_server.println("t");
-    //Serial.println("t");
-    }
-    next_send+= 10;
-    esp_server.available();
+  }
+
+  if(millis > next_send && closed){
+    closed = esp_server.connected();
+    Serial.println("Reconnected");
   }
   
+  if(curr_connected){
+    Serial.println("Connected");
+  }
+  if (key && curr_connected) // Check for a valid key.
+  {
+    esp_server.println(key);
+    Serial.println(key);
+  }
+  if(millis() > next_send && curr_connected && !closed) {
+    if(esp_server.connected()){
+      esp_server.println("t");
+      Serial.println("t");
+    }
+    
+    //Serial.println(millis());
+    next_send = millis() + 50;
+    //esp_server.available(); 
+    
+  }
+  if (!curr_connected){
+    Serial.println("no connection");
+  }
+
 }
 
--- Keypad_alt.py
+++ Keypad_alt.py
@@ -5,38 +5,28 @@ from tkinter import ttk
 import socket as so
 import time
 
-#Buttons= []
-#cur_button= 0
+Buttons= []
+cur_button= 0
 
 class Arduino(object):
     def __init__(self, window):
+        #self.arduino = serial.Serial('COM7',9600,timeout=0.1)
         self.window= window
-        self.buffer= bytes()
-        self.connected= False
-        self.socket= so.socket()
 
+        #self.socket= so.socket()
         self.connect_window= Connect_window(self)
-       
-    def __del__(self):
-        try:
-            self.close()
-        except OSError:
-            pass
+
+        self.errors= 0
+        self.buffer= bytes()
+
         
 
     def close(self):
-        self.socket.recv(1024)
-        self.socket.send('off'.encode('utf-8') + b'\n')
-        print('Tried to close the connection')
-        self.socket.recv(1024)
-        time.sleep(50)
-        self.socket.recv(1024)
         self.socket.close()
-        self.connected= False
 
     def new_connection(self, host, port):
-        self.socket= so.socket()
         try:
+            self.socket= so.socket()
             self.socket.connect((host, port))
             self.socket.setblocking(False)
 
@@ -44,15 +34,14 @@ class Arduino(object):
             self.listen()
         except OSError:
             self.connect_window= Connect_window(self)
-            print('Error')
-        
+            print('Connection Error')
+
     def listen(self):
         try:
             read = self.socket.recv(1024)
             self.buffer+= read
-            self.connected= True
             
-        except:
+        except so.error:
             self.errors+= 1
         
             if self.errors>500:
@@ -67,15 +56,12 @@ class Arduino(object):
             if read=="t":
                 self.errors= 0
             else:
-                try:
-                    i= int(read)-1
-                    if 0<=i<=15:
-                        self.window.buttons[i].exec_command()
-                except ValueError:
-                    pass
+                i= int(read)-1
+                if 0<=i<=15:
+                    Buttons[i].exec_command()
             
         if self.errors<= 500:
-            self.window.window.after(5,self.listen)
+            self.window.after(10,self.listen)
        
 class Connect_window(object):
     def __init__(self, arduino):
@@ -92,13 +78,8 @@ class Connect_window(object):
         self.ip_field.insert(0, 'Enter IP-Adress')
         self.port_field.insert(0, 'Enter Port')
 
-
     def __del__(self):
-        try:
-            self.connect_window.destroy()
-        except:
-            pass
-        
+        self.connect_window.destroy()
 
     def connect(self):
         host= self.ip_field.get()
@@ -110,51 +91,46 @@ class Connect_window(object):
 class Window(object):
     def __init__(self):
         self.window= tk.Tk()
-        self.window.title('Keypad')
-
-        self.cur_button= 0
-        self.buttons= []
                 
         self.label1 = tk.Label(self.window, text="Neue Belegung für Taste:")
         self.label1.grid(row=4,column=0, columnspan= 2)
-        self.text_field= Text_field(self)
-        self.command_checkbutton= Command_checkbutton(self)
-        self.save_button= Save_button(self, self.text_field, self.command_checkbutton)
-        self.keypad= Keypad(self, self.text_field, self.command_checkbutton)  #Erzeugt Keypad
+        self.text_field= Text_field(self.window)
+        self.command_checkbutton= Command_checkbutton(self.window)
+        self.save_button= Save_button(self.window, self.text_field, self.command_checkbutton)
+        self.keypad= Keypad(self.window, self.text_field, self.command_checkbutton)  #Erzeugt Keypad
 
-        self.arduino= Arduino(self)                
+
+        self.arduino= Arduino(self.window)                
         self.window.mainloop()
+    
             
 
 class Keypad(object):   #Erzeugt die Buttons des Keypads
      def __init__(self, window, text_field, command_checkbutton):
-        self.window= window
         self.keys=[     #Beschriftungen der Buttons
             ['1','2','3','A'],
             ['4','5','6','B'],
             ['7','8','9','C'],
             ['*','0','#','D'] 
         ]
-
         for y in range(4):      #Erzeugt die Buttons
             for x in range(4):
-                self.window.buttons.append(Button(self.window, text_field, command_checkbutton, name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
+                Buttons.append(Button(window, text_field, command_checkbutton, name= self.keys[y][x], x=x, y=y, index=x+(4*y)))  #Werden in Liste gespeichert
         
 class Button (object):
     def __init__(self, window, text_field, command_checkbutton, name, x, y, index):
-        self.window= window
+        style= tk.ttk.Style()
+        style.configure("Active_Button.TButton", foreground="black", background="blue")
+        self.button=tk.ttk.Button(window, command= self.set_cur_button, text= name) #Erzeugt Button über tkinter
+        self.button.grid(row= y, column= x)     
+        self.button_text= ''   #geespeicherter Text, der geändert werden kann
+
         self.index= index   #Position in der Liste
+        self.window= window
         self.text_field= text_field
         self.command_checkbutton= command_checkbutton
         self.is_command= 0
 
-        style= tk.ttk.Style()
-        style.configure("Active_Button.TButton", foreground="black", background="blue")
-        self.button=tk.ttk.Button(self.window.window, command= self.set_cur_button, text= name) #Erzeugt Button über tkinter
-        self.button.grid(row= y, column= x)     
-        self.button_text= ''   #geespeicherter Text, der geändert werden kann
-
-        
     def change_text(self, new_text, is_command):    #ändert den Text
         self.button_text= new_text
         self.is_command= is_command
@@ -175,30 +151,30 @@ class Button (object):
         return self.button_text
 
     def set_cur_button(self):      #Setzt den angeklickten Button als aktuell ausgewählten Button, der bearbeitet werden soll
-        self.window.buttons[self.window.cur_button].button.configure(style= "TButton")
-        self.window.cur_button=self.index
-        self.window.buttons[self.window.cur_button].button.configure(style= "Active_Button.TButton")
+        global cur_button
+        Buttons[cur_button].button.configure(style= "TButton")
+        cur_button=self.index
+        Buttons[cur_button].button.configure(style= "Active_Button.TButton")
         self.text_field.update_text(self.button_text)      #Zeigt den Text des ausgewählten Buttons im Textfeld an
         self.command_checkbutton.update_checked(self.is_command)
 
 class Save_button (object):     #Button zum speichern
     def __init__(self, window, text_field, command_checkbutton):     #Erzeut Button
-        self.window= window
-        save_button= tk.ttk.Button(window.window, command= self.save_text, text= 'Save')
+        save_button= tk.ttk.Button(window, command= self.save_text, text= 'Save')
         save_button.grid(row=5, column=2)
         self.text_field= text_field
         self.command_checkbutton= command_checkbutton
 
     def save_text(self):       #Speichert den eingegebenen Text im Button
-        #global cur_button
+        global cur_button
         text= self.text_field.text_field.get()
         is_command= self.command_checkbutton.checked.get()
-        self.window.buttons[self.window.cur_button].change_text(text, is_command)
+        Buttons[cur_button].change_text(text, is_command)
 
         
 class Text_field (object):
     def __init__(self, window):
-        self.text_field= tk.Entry(window.window) #Entry ist im Gegensatz zu Text von Anfang an sichtbar.
+        self.text_field= tk.Entry(window) #Entry ist im Gegensatz zu Text von Anfang an sichtbar.
         self.text_field.grid(row= 5, column= 0, columnspan= 2)
         
     def update_text(self, text):# Ändert den gegebenen Text
@@ -208,7 +184,7 @@ class Text_field (object):
 class Command_checkbutton(object):
     def __init__(self, window):
         self.checked= tk.IntVar()
-        self.checkbutton= tk.ttk.Checkbutton(window.window, text= 'Command', variable= self.checked)
+        self.checkbutton= tk.ttk.Checkbutton(window, text= 'Command', variable= self.checked)
         self.checkbutton.grid(row=5, column= 3)
         
     def update_checked(self, checked):
