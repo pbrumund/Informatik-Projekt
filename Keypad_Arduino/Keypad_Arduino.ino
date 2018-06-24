d3b8d2df73da55b60557434c469cd48d6b0c8347
Patrick Brumund <patrickbrumund@gmail.com>
2018-06-24 11:39:24 +0200

    aktuellste Kommentare, jetzt auch für Arduino

--- Keypad_Arduino/Keypad_Arduino.ino
+++ Keypad_Arduino/Keypad_Arduino.ino
@@ -1,15 +1,21 @@
+//Bibliothek für Keypad
 #include "Key.h"
 #include "Keypad.h"
 #include <SoftwareSerial.h>
 #include "DumbServer.h"
 
-
+//Anzahl der Reihen des Keypads
 const byte ROWS = 4;
+//Anzahl der Spalten des Keypads
 const byte COLS = 4;
+//Zeitpunkt, zu dem das nächste Testbyte gesendet wird, in ms
 unsigned long next_send = 0;
+//Wird auf wahr gesetzt, wenn Nachricht empfangen wird
 bool receiving = false;
 
 
+//Zahlen, die gesendet werden, wenn Taste gedrückt wird, Konfiguration des Keypads
+//Eigentlich char, uint8_t hat jedoch selbe Länge
 uint8_t keys[ROWS][COLS] = {
   {1, 2, 3, 4},
   {5, 6, 7, 8},
@@ -17,16 +23,25 @@ uint8_t keys[ROWS][COLS] = {
   {13, 14, 15, 16}
 };
 
+//Pins, an denen die Reihen des Keypads angeschlossen sind, Reihenfolge wie Anschlüsse an Keypad
 byte rowPins[ROWS] = {4, 5, 6, 7};
+//Pins, an denen die Spalten des Keypads angeschlossen sind, Reihenfolge wie Anschlüsse an Keypad
 byte colPins[COLS] = {10, 11, 12, 13};
 
+
+//Erstellt ein Keypad aus der Keypad-Bibliothek mit den gegebenen Belegungen, Reihen und Spalten
+//Gibt später mit get_key aus, ob eine Taste gedrückt wurde
 Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
+//Verbindet mit WLAN-Shield
 SoftwareSerial esp_serial(3, 2);
+//Erstellt Server
 EspServer esp_server;
 
+//Adresse des Resets, um bei Schließen der Verbindung am Computer neu zu starten und Abstürzen zu verhindern
 void(*resetFunc)(void) = 0;
 
+//Überprüft, ob der Computer Daten gesendet hat, wenn der String "off" gesendet wurde, wird der Arduino neu gestartet
 void check_off_msg() {
   while (esp_server.available() || receiving) {
     receiving = true;
@@ -37,11 +52,14 @@ void check_off_msg() {
   }
 }
 
+//Sendet den übermittelten Key (Nummer der Taste, deklariert in keys) an den Computer
 void send_pressed_button(uint8_t key) {
   esp_server.println(key);
-  //Serial.println(key);
+  //Debugging, nur für Serial Monitor
+  Serial.println(key);
 }
 
+//Sendet ein "t" an den Computer, um mitzuteilen, dass Arduino noch online ist
 void send_test() {
   esp_server.println("t");
   //Serial.println("t");
@@ -49,31 +67,38 @@ void send_test() {
 
 void setup()
 {
+  //Startet Verbindung über USB, um IP auszugeben, eventuell Debugging
   Serial.begin(9600);
+  //Verbindet mit WLAN-Shield
   esp_serial.begin(9600);
   Serial.println("Starting server...");
-  esp_server.begin(&esp_serial, "FRITZ!Box Fon WLAN 7390", "byron1234", 30303);
+  //Startet Server auf Port 30303 mit angegebener SSID und Passwort
+  esp_server.begin(&esp_serial, "arduino", "password", 30303);
   Serial.println("...server is running");
-  /* Get and print the IP-Address the python program
-     should connect to */
+  //Speichert die IP, um sie mitzuteilen
   char ip[16];
   esp_server.my_ip(ip, 16);
-
+  //Gibt IP-Adresse aus
   Serial.print("My ip: ");
   Serial.println(ip);
 }
 
 void loop()
 {
+  //Überprüft, ob der Computer mit dem Server verbunden ist
   bool curr_connected = esp_server.connected();
+  //Überprüft, ob eine Taste gedrückt wurde, falls dies der Fall ist, wird der in keys gespeicherte Wert in der Variable gespeichert
   uint8_t key = kpd.getKey();
 
+  //Überprüft, ob Arduino sich neu starten soll
   check_off_msg();
 
+  //Falls eine Taste gedrückt wurde und diese gesendt werden kann, wird sie dem Computer übermittelt
   if (key && curr_connected) {
     send_pressed_button(key);
   }
 
+  //Falls der Zeitpunkt für das nächste Testbit erreicht ist, wird diese gesendet und next_send um 50ms erhöht
   if (millis() > next_send && curr_connected) {
     send_test();
     next_send = millis() + 50;
--- Keypad_GUI.py
+++ Keypad_GUI.py
@@ -9,38 +9,36 @@ import json
 class Arduino(object):
     
     ##Erstellt einen Arduino
-    #@param window Objekt der Klasse Window
-    
+    #@param window Objekt der Klasse Window    
     def __init__(self, window):
-        ##@brief Objekt der Klasse Window zum Zugreifen auf dessen Parameter
+        ##Objekt der Klasse Window zum Zugreifen auf dessen Parameter
         self.window= window
         
-        ##@brief Variable zum Zwischenspeichern der empfangenen Daten
+        ##Variable zum Zwischenspeichern der empfangenen Daten
         self.buffer= bytes()
-        ##@brief gibt an, ob Verbindung besteht
+        ##gibt an, ob Verbindung besteht
         self.connected= False
-        #Erstellt Fenster zum Verbinden
+        ##Erstellt Fenster zum Verbinden
         self.connect_window= Connect_window(self)
 
     ## Schließt beim Löschen die Verbindung
     def __del__(self):
         self.close()
 
-    """
-    Sendet Nachricht an den Arduino, dass Verbndung geschlossen werden soll,
-    dieser startet sich neu, um nicht abzustürzen.
-    """
-    def send_off(self):
-        
+    
+    ##Sendet Nachricht an den Arduino, dass Verbindung geschlossen werden soll,
+    ##dieser startet sich neu, um nicht abzustürzen.
+    def send_off(self):        
         try:
             self.socket.send('off'.encode('utf-8') + b'\n')
-            self.window.window.after(20, self.send_off) #Nachricht wird nicht jedes mal empfangen, also wird Text mehrmals gesendet
+            #Nachricht wird nicht jedes mal empfangen, also wird Text mehrmals gesendet
+            self.window.window.after(20, self.send_off)
         except:
             pass
         
     ##Schließt die Verbindung zum Arduino
     def close(self):
-        ##@brief Setzt den Zustand auf nicht verbunden
+        ##Setzt den Zustand auf nicht verbunden
         self.connected= False
         try:
             #Stoppt, Nachrichten zu senden
@@ -52,13 +50,12 @@ class Arduino(object):
             #Erscheint, wenn beim Schließen keine Verbinfung besteht
             print('Was not able to close the connection')
         
-    """
-    Methode, die die Verbindung mit dem Arduino herstellt.
-    @param host IP-Adresse des Arduinos
-    @param port Port des Arduinos
-    """
+    
+    ##Methode, die die Verbindung mit dem Arduino herstellt.
+    #@param host IP-Adresse des Arduinos
+    #@param port Port des Arduinos
     def new_connection(self, host, port):   
-        ##@brief Erstellt ein Objekt der Klasse Socket
+        #Erstellt ein Objekt der Klasse Socket
         self.socket= so.socket()
         try:
             #Versucht, Verbindung herzustellen
@@ -75,12 +72,10 @@ class Arduino(object):
             #Erzeugt neues Fenster zum Verbinden
             self.connect_window= Connect_window(self)
             print('Error')
-        
+    
+    ##Empfängt vom Arduino versendete Daten und stellt fest, ob 
+    ##überhaupt eine fehlerfreie Verbindung mit dem Arduino besteht.  
     def listen(self):
-        """
-        Empfängt vom Arduino versendete Daten und stellt fest, ob 
-        überhaupt eine fehlerfreie Verbindung mit dem Arduino besteht.
-        """
         try:
             #Empfängt gesendete Daten, speichert sie im Buffer
             read = self.socket.recv(1024)
@@ -89,11 +84,9 @@ class Arduino(object):
         except:
             #Arduino hat keine Daten gesendet
             self.errors+= 1      
-            if self.errors>500: 
-                """   
-                Wenn die Anzahl der gescheiterten Empfangsversuche 500 übersteigt, also 2,5s nichts empfangen wurde,
-                wird eine Fehlermeldung ausgegeben und das Verbindungsfenster wieder aufgerufen.
-                """
+            if self.errors>500:   
+                #Wenn die Anzahl der gescheiterten Empfangsversuche 500 übersteigt, also 2,5s nichts empfangen wurde,
+                #wird eine Fehlermeldung ausgegeben und das Verbindungsfenster wieder aufgerufen.
                 print('Connection failed') 
                 self.close()
                 self.connect_window= Connect_window(self)
@@ -127,16 +120,14 @@ class Arduino(object):
 
 
 class Connect_window(object):
-    """
-    Definiert und erzeugt das Verbindungsfenster, dass zeitgleich mit dem Aufruf des eigentlichen
-    Bedienfensters aufgerufen wird um die IP-Adresse und den Port für die aufzubauende 
-    Verbindung festzulegen.
-    @param arduino Objekt der Klasse Arduino 
-    """
+    ##Definiert und erzeugt das Verbindungsfenster, dass zeitgleich mit dem Aufruf des eigentlichen
+    ##Bedienfensters aufgerufen wird um die IP-Adresse und den Port für die aufzubauende 
+    ##Verbindung festzulegen.
+    #@param arduino Objekt der Klasse Arduino 
     def __init__(self, arduino):
-        ##@brief Speichert den gegebenen Arduino
+        ##Speichert den gegebenen Arduino
         self.arduino= arduino
-        #Erzeugt Fenster
+        ##Tkinter-Toplevel
         self.connect_window= tk.Toplevel()
         #Verschiebt Fenster nach oben
         self.connect_window.attributes("-topmost", True)
@@ -154,21 +145,16 @@ class Connect_window(object):
         self.port_field.grid(column= 1, row= 1)
         self.connect_button.grid(column= 1, row= 2)
 
+    ##Schließt das Verbindungsfenster nach Eingabe der Verbindungsdaten(IP,Port)
     def __del__(self):
-        """
-        Schließt das Verbindungsfenster nach Eingabe der Verbindungsdaten(IP,Port)
-        """
-
         try:
             self.connect_window.destroy()
         except:
             pass        
 
+    ##Stellt die Verbindung mit dem Arduino her, indem es die Verbindungsdaten
+    ##an die "new-connection"-Methode der Klasse Arduino weitergibt, schließt sich danach selbst.
     def connect(self):
-        """
-        Stellt die Verbindung mit dem Arduino her, indem es die Verbindungsdaten
-        an die "new-connection"-Methode der Klasse Arduino weitergibt.
-        """
         try:
             host= self.ip_field.get()
             port= int(self.port_field.get())
@@ -180,19 +166,18 @@ class Connect_window(object):
 
 
 class Window(object):
-    """
-    Definiert und erzeugt das Hauptfenster. Inhalte des Hauptfensters sind:
-    Keypad(keypad), Textfeld(text_field) , Speicherknopf(save_button) und Checkbox(command_checkbutton)
-    Profilspeicherknopf(json_button), Profilladeknopf(load_button).
-    """
+    ##Definiert und erzeugt das Hauptfenster. Inhalte des Hauptfensters sind:
+    ##Keypad(keypad), Textfeld(text_field) , Speicherknopf(save_button) und Checkbox(command_checkbutton)
+    ##Profilspeicherknopf(json_button), Profilladeknopf(load_button).
     def __init__(self):
+        ##Tkinter-Hauptfenster
         self.window= tk.Tk()
         self.window.title('Keypad')
         #Ändert die Routine, die beim Schließen des Fensters ausgeführt wird, um den Arduino zu benachrichtigen
         self.window.protocol("WM_DELETE_WINDOW", self.on_closed)
-        ##@brief Der aktuell zum Bearbeiten ausgewählte Button
+        ##Der aktuell zum Bearbeiten ausgewählte Button
         self.cur_button= 0
-        ##@brief Liste, in der die Button-Objekte gespeichert sind
+        ##Liste, in der die Button-Objekte gespeichert sind
         self.buttons= []
 
         #Erzeugt Label     
@@ -227,6 +212,7 @@ class Window(object):
         #Wartet bis zum Schließen des Fensters eine Sekunde, damit Nachricht empfangen wird
         self.window.after(1000, self.__del__)
 
+    #Schließt das Fenster, wird 1s nach Klicken des Schließen-Buttons aufgerufen
     def __del__(self):
         try:
             self.window.destroy()
@@ -235,10 +221,8 @@ class Window(object):
 
 
 class Keypad(object):   
-    """
-    Die Klasse Keypad definiert, wie die Buttons im späteren Programm
-    angeordnet sind und erzeugt sie.
-    """
+    ##Die Klasse Keypad definiert, wie die Buttons im späteren Programm
+    ##angeordnet sind und erzeugt sie.
     def __init__(self, window):
         self.window= window
         ##Anordnung der Buttons mit Beschriftung
@@ -248,56 +232,48 @@ class Keypad(object):
             ['7','8','9','C'],
             ['*','0','#','D'] 
         ]
-        ##Erzeugt die Buttons und speichert sie in zum Fenster gehörenden Liste
+        #Erzeugt die Buttons und speichert sie in zum Fenster gehörenden Liste
         for y in range(4):      
             for x in range(4):
                 self.window.buttons.append(Button(self.window, label= self.keys[y][x], x=x, y=y+1, index=x+(4*y)))  #Werden in Liste gespeichert
 
 
 class Button (object):
-    """
-    Die Klasse Button definiert den Aufbau eines Buttons und die dazugehörigen Parameter.
-    Die Buttons werden im ttk-Style dargestellt.
-    Jedem Button kann ein Text zugeordnet werden, die Variable is_command gibt mit 0 = Nein
-    und 1 = Ja an, ob es sich hierbei um  einen Tastatur-Kurzbefehl handelt.
-    """
-    ##Konstruktor
+    ##Die Klasse Button definiert den Aufbau eines Buttons und die dazugehörigen Parameter.
+    ##Die Buttons werden im ttk-Style dargestellt.
+    ##Jedem Button kann ein Text zugeordnet werden, die Variable is_command gibt mit 0 = Nein
+    ##und 1 = Ja an, ob es sich hierbei um  einen Tastatur-Kurzbefehl handelt.
     #@param window Objekt der Klasse Window
     #@param label Beschriftung, die dargestellt wird
     #@param x x-Koordinate
     #@param y y-Koordinate
     #@param index Index in der Liste
     def __init__(self, window, label, x, y, index):
-        #Fenster, in dem sich Buttons befinden, für Zugriff auf Variablen/Funtionen
+        ##Fenster, in dem sich Buttons befinden, für Zugriff auf Variablen/Funtionen
         self.window= window
-        #Position in der Liste
+        ##Position in der Liste
         self.index= index 
-        #Gibt an, ob es sich um Tastenkombinatioin handelt
+        ##Gibt an, ob es sich um Tastenkombinatioin handelt
         self.is_command= 0 
-        #gespeicherter Text, der geändert werden kann, dieser wird beim Drücken des Buttons am Auduino getippt bzw. als Tastenkombination ausgeführt   
+        ##gespeicherter Text, der geändert werden kann, 
+        ##dieser wird beim Drücken des Buttons am Auduino getippt bzw. als Tastenkombination ausgeführt   
         self.button_text= ''   
         #Erzeugt ttk-Style
         style= tk.ttk.Style()
         style.configure("Active_Button.TButton", foreground="black", background="blue")
 
-        #Erzeugt Button über tkinter
+        ##Erzeugt Button über Tkinter
         self.button=tk.ttk.Button(self.window.window, command= self.set_cur_button, text= label) 
         self.button.grid(row= y, column= x)     
         
-    
+    ##Methode mit der der dem Button zugewiesene Text geändert wird.
     def change_text(self, new_text, is_command):   
-        """
-        Methode mit der der dem Button zugewiesene Text geändert wird.
-        """
         self.button_text= new_text
         self.is_command= is_command
-        
+
+    ##Wenn der zugewiesene Text ein Tastatur-Kurzbefehl sein soll, wird mit dieser Methode bewirkt, dass er auch als solcher 
+    ##ausgeführt wird. Ist der Text keine Tastenkombination, wird er über die typewrite-Methode von Pyautogui eingegeben
     def exec_command(self):
-        """
-        Wenn der zugewiesene Text ein Tastatur-Kurzbefehl sein soll,
-        wird mit dieser Methode bewirkt, dass er auch als solcher 
-        ausgeführt wird.
-        """
         if self.is_command:     
             """
             Führt Tastenkombination aus. Der dem Button zugewiesene Text wird
@@ -315,11 +291,9 @@ class Button (object):
             """
             key.typewrite(self.button_text, interval= 0.00)
 
+    ##Setzt den angeklickten Button als aktuell ausgewählten Button, der bearbeitet werden soll.
+    ##Der dem Button aktuell zugewiesene Text wird im Textfeld angezeigt.
     def set_cur_button(self): 
-        """
-        Setzt den angeklickten Button als aktuell ausgewählten Button, der bearbeitet werden soll.
-        Der dem Button aktuell zugewiesene Text wird im Textfeld angezeigt.
-        """
         self.window.buttons[self.window.cur_button].button.configure(style= "TButton")
         self.window.cur_button=self.index
         self.window.buttons[self.window.cur_button].button.configure(style= "Active_Button.TButton")
@@ -328,24 +302,20 @@ class Button (object):
 
 
 class Json_save_button (object):
-    """
-    Definiert und erzeugt einen Button um das Speichern von Profilen zu ermöglichen.
-    """
+    ##Definiert und erzeugt einen Button um das Speichern von Profilen zu ermöglichen.
     def __init__(self,window):
         self.window= window
         self.json_button = tk.ttk.Button(window.window, command=self.save_json,text= 'Profil speichern')
         self.json_button.grid(row=0, column=0, columnspan= 2, sticky= tk.W+tk.E)
         
-
+    
+    ##Methode, mit der der den Einzelnen Tasten zugewiesene Text und der jeweilige
+    ##Status(Bezogen auf 'Is_command') in einer Datei gespeichert wird. Hierfür wird 
+    ##eine Liste(json_list) mit den Tastenbelegungen angelegt und eine Liste(is_command_list) 
+    ##mit dem Status der Tasten(bezogen auf die "Command"-Checkbox) angelegt. Die beiden Listen    
+    ##werden in ein Dictionary(write_to_json) zusammengefügt und im Json-Format gespeichert. 
+    ##Die Wahl des Dateinamens geschieht mithilfe des Tkinter-Moduls Filedialog.
     def save_json(self):
-        """
-        Methode, mit der der den Einzelnen Tasten zugewiesene Text und der jeweilige
-        Status(Bezogen auf 'Is_command') in einer Datei gespeichert wird. Hierfür wird 
-        eine Liste(json_list) mit den Tastenbelegungen angelegt und eine Liste(is_command_list) 
-        mit dem Status der Tasten(bezogen auf die "Command"-Checkbox) angelegt. Die beiden Listen
-        werden in ein Dictionary(write_to_json) zusammengefügt und im Json-Format gespeichert. 
-        Die Wahl des Dateinamens geschieht mithilfe des Tkinter-Moduls Filedialog.
-        """
         json_list = []
         is_command_list = []
         write_to_json = {}
@@ -366,23 +336,17 @@ class Json_save_button (object):
                 
 
 class Json_load_button(object):
-    """
-    Definiert und erzeugt einen Button um das Laden von Profilen zu ermöglichen.
-    """
+    ##Definiert und erzeugt einen Button um das Laden von Profilen zu ermöglichen.
     def __init__(self,window):
         self.window= window
         self.load_button = tk.ttk.Button(window.window, command = self.load,text= 'Profil laden')
         self.load_button.grid(row=0, column=2, columnspan= 2, sticky= tk.W+tk.E)
 
-    def load(self):
-
-        """
-        Die im Profil im Json-Format gespeicherten Daten werden in ein temporäres Dictionary("dictionary") geladen und
-        anschließend in zwei Listen("Content" und "Is_Command")  aufgeteilt. Anschließend wird den Tasten der 
-        im jeweiligen Profil abgelegte Inhalt und Status mit einer For-Schleife zugewiesen. Die Auswahl der zu 
-        ladenden Datei geschieht mithilfe des Tkinter-Moduls Filedialog.
-        """
-        
+    ##Die im Profil im Json-Format gespeicherten Daten werden in ein temporäres Dictionary("dictionary") geladen und
+    ##anschließend in zwei Listen("Content" und "Is_Command")  aufgeteilt. Anschließend wird den Tasten der   
+    ##im jeweiligen Profil abgelegte Inhalt und Status mit einer For-Schleife zugewiesen. Die Auswahl der zu 
+    ##ladenden Datei geschieht mithilfe des Tkinter-Moduls Filedialog.
+    def load(self):        
         content = []
         iscmd = []
 
@@ -404,38 +368,33 @@ class Json_load_button(object):
 
 
 class Save_button (object):     
-    """
-    Die Klasse Save_button definiert und erzeugt den im Programmfenster angezeigten Button mit der Beschriftung 'Save', 
-    mit dem der im Textfeld eingegebene Text dem ausgewählten Button zugewiesen wird.
-    @param window Objekt der Klasse Window
-    """
+    ##Die Klasse Save_button definiert und erzeugt den im Programmfenster angezeigten Button mit der Beschriftung 'Save', 
+    ##mit dem der im Textfeld eingegebene Text dem ausgewählten Button zugewiesen wird.
+    #@param window Objekt der Klasse Window
     def __init__(self, window):     
         self.window= window
         self.save_button= tk.ttk.Button(window.window, command= self.save_text, text= 'Speichern')
         self.save_button.grid(row=6, column=2)
         
-
+    ##Speichert die im Textfeld und Checkbutton eingegebenen Daten in dem aktuell ausgewählten Button
     def save_text(self):       
-        """
-        Dem Button Save_button zugewiesenes Kommando, speichert den eingegebenen Text im Button.
-        """
+        #Dem Button Save_button zugewiesenes Kommando, speichert den eingegebenen Text im Button.
         text= self.window.text_field.text_field.get()
         is_command= self.window.command_checkbutton.checked.get()
+        #Speichert eingegebene Daten, indem die change_text-Methode des aktuellen Buttons aufgerufen wird
         self.window.buttons[self.window.cur_button].change_text(text, is_command)
 
 
 class Text_field (object):
-    """
-    Die Klasse Textfield definiert und erzeugt das im Programmfenster angezeigte Textfeld. Hier wird der Text eingegeben,
-    der dem ausgewählten Button zugewiesen werden soll. Wird ein Button ausgewählt wird hier zudem der dem Button bisher
-    zugeordnete Text angezeigt.
-    @param window Objekt der Klasse Window
-    """
+    ##Die Klasse Textfield definiert und erzeugt das im Programmfenster angezeigte Textfeld. Hier wird der Text eingegeben,
+    ##der dem ausgewählten Button zugewiesen werden soll. Wird ein Button ausgewählt wird hier zudem der dem Button bisher
+    ##zugeordnete Text angezeigt.
+    #@param window Objekt der Klasse Window
     def __init__(self, window):
         self.text_field= tk.Entry(window.window) 
         self.text_field.grid(row= 6, column= 0, columnspan= 2)
 
-    # Ändert den gegebenen Text, um Text des neu ausgewählten Buttons anzuzeigen       
+    ##Ändert den gegebenen Text, um Text des neu ausgewählten Buttons anzuzeigen       
     def update_text(self, text):
         #Leert das Feld
         self.text_field.delete(0,'end') 
@@ -456,7 +415,8 @@ class Command_checkbutton(object):
         self.checkbutton= tk.ttk.Checkbutton(window.window, text= 'Command', variable= self.checked)
         self.checkbutton.grid(row=6, column= 3)
 
-    #Ändert den Zustand, wird aufgerufen, wenn neuer Button ausgewählt wird  
+    ##Ändert den Zustand, wird aufgerufen, wenn neuer Button ausgewählt wird 
+    #@param checked Wert, auf den checked gesetzt werden soll
     def update_checked(self,checked):
         self.checked.set(checked)
 
